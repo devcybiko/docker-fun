712ea901e86ec4b2a36e2b4bff2e6378defbe607
Gregory Smith <greg@agilefrontiers.com>
2024-03-30 11:18:39 -0400

    got debug working

--- projects/ibookintosh/src/main.c
+++ projects/ibookintosh/src/main.c
@@ -1,5 +1,7 @@
 #define TRACE_OFF
 
+#include "main.h"
+
 #include "GObj.h"
 #include "GList.h"
 #include "GMap.h"
@@ -11,22 +13,23 @@
 
 typedef struct Context
 {
-    char *message;
+    char* message;
     int counter;
 } Context;
 
-void *newContext(char *message, int counter)
+void* newContext(char* message, int counter)
 {
-    Context *context = NEW(Context);
+    Context* context = NEW(Context);
     context->message = message;
     context->counter = counter;
     return context;
 }
 
-int cronPrint(GCronEntry *entry)
+int cronPrint(GCronEntry* entry)
 {
+    blink(75, 500);
     TRACE("cronPrint: %p\n", entry);
-    Context *context = (Context *)entry->context;
+    Context* context = (Context*)entry->context;
     TRACE("cronPrint: context=%p\n", entry->context);
     printf("cronPrint: %s, %d\n", context->message, context->counter);
     context->counter--;
@@ -38,42 +41,53 @@ int cronPrint(GCronEntry *entry)
     TRACE("  %s leaving cronPrint\n\n", context->message);
     return 0;
 }
-
-int main(int argc, char const *argv[])
+int blink(int onms, int offms)
 {
-    printf("Hello, World!\n");
-    GObj *obj = GObj_new();
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
+    sleep_ms(onms);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(offms);
+}
+
+int main(int argc, char const* argv[])
+{
+    stdio_init_all();
+
+    GObj* obj = GObj_new();
     _(obj).debug("GObj Test");
     printf("%s\n", _(obj).toString());
     _(obj).delete();
 
-    GList *list = GList_new();
+    GList* list = GList_new();
     _(list).push("zero");
     _(list).push("one");
     _(list).push("two");
     _(list).debug("GList Test");
-    printf("%s\n", (char *)_(list).get(0));
-    printf("%s\n", (char *)_(list).get(1));
-    printf("%s\n", (char *)_(list).get(2));
+    printf("%s\n", (char*)_(list).get(0));
+    printf("%s\n", (char*)_(list).get(1));
+    printf("%s\n", (char*)_(list).get(2));
     _(list).delete();
     printf("\n");
 
-    GMap *map = GMap_new();
+    GMap* map = GMap_new();
     _(map).put("zero", "cero");
     _(map).put("one", "uno");
     _(map).put("two", "dos");
     _(map).debug("GMap Test");
-    printf("%s\n", (char *)_(map).get("zero"));
-    printf("%s\n", (char *)_(map).get("one"));
-    printf("%s\n", (char *)_(map).get("two"));
+    printf("%s\n", (char*)_(map).get("zero"));
+    printf("%s\n", (char*)_(map).get("one"));
+    printf("%s\n", (char*)_(map).get("two"));
     _(map).delete();
     printf("\n");
 
-    GCronEntry *entry = GCronEntry_new(1000, cronPrint, newContext("SLOW!", 10));
+    GCronEntry* entry = GCronEntry_new(1000, cronPrint, newContext("SLOW!", 10));
     _(entry).debug("GCronEntry Test");
     _(entry).delete();
 
-    GCron *cron = GCron_new();
+    GCron* cron = GCron_new();
     _(cron).add(250, cronPrint, newContext("FAST!", 10));
     _(cron).add(500, cronPrint, newContext("MEDIUM!", 10));
     _(cron).add(1000, cronPrint, newContext("SLOW!", 10));
